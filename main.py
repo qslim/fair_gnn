c5b5f53a6ddc4234355f82e7eea8b917ebad8b76
“qslim” <mqyang1s@163.com>
2023-10-18 11:24:53 +0800

    update

--- main.py
+++ main.py
@@ -44,6 +44,21 @@ def accuracy(output, labels):
     return correct / len(labels)
 
 
+def fair_metric(output, idx, labels, sens):
+    val_y = labels[idx].cpu().numpy()
+    idx_s0 = sens.cpu().numpy()[idx.cpu().numpy()] == 0
+    idx_s1 = sens.cpu().numpy()[idx.cpu().numpy()] == 1
+
+    idx_s0_y1 = np.bitwise_and(idx_s0, val_y == 1)
+    idx_s1_y1 = np.bitwise_and(idx_s1, val_y == 1)
+
+    pred_y = (output[idx].squeeze() > 0).type_as(labels).cpu().numpy()
+    parity = abs(sum(pred_y[idx_s0]) / sum(idx_s0) - sum(pred_y[idx_s1]) / sum(idx_s1))
+    equality = abs(sum(pred_y[idx_s0_y1]) / sum(idx_s0_y1) - sum(pred_y[idx_s1_y1]) / sum(idx_s1_y1))
+
+    return parity, equality
+
+
 def main_worker(args, config):
     print(args, config)
     seed_everything(args.seed)
@@ -52,7 +67,7 @@ def main_worker(args, config):
 
     # Load the dataset and split
     pokec = POKEC(dataset_sample='pokec_z')  # you may also choose 'pokec_n'
-    adj, x, y, idx_train, idx_val, idx_test, sens = pokec.adj, pokec.features, pokec.labels, pokec.idx_train, pokec.idx_val, pokec.idx_test, pokec.sens
+    adj, x, labels, idx_train, idx_val, idx_test, sens = pokec.adj, pokec.features, pokec.labels, pokec.idx_train, pokec.idx_val, pokec.idx_test, pokec.sens
 
     # feature_normalize
     # x = np.array(x)
@@ -78,43 +93,42 @@ def main_worker(args, config):
     for epoch in range(config['epoch']):
         net.train()
         optimizer.zero_grad()
-        logits = net(e, u, x)
-        loss = F.binary_cross_entropy_with_logits(logits[idx_train], y[idx_train].unsqueeze(1).float())
+        output = net(e, u, x)
+        loss = F.binary_cross_entropy_with_logits(output[idx_train], labels[idx_train].unsqueeze(1).float())
+        acc_train = accuracy(output[idx_train], labels[idx_train])
         loss.backward()
         optimizer.step()
 
         net.eval()
-        logits = net(e, u, x)
-        acc_val = accuracy(logits[idx_val], y[idx_val])
-        acc_test = accuracy(logits[idx_test], y[idx_test])
-        parity, equality = fair_metric(logits, idx_test, y, sens)
-        # print(100 * acc_test, 100 * parity, 100 * equality)
+        output = net(e, u, x)
+        acc_val = accuracy(output[idx_val], labels[idx_val])
+        acc_test = accuracy(output[idx_test], labels[idx_test])
+        parity_val, equality_val = fair_metric(output, idx_val, labels, sens)
+        parity_test, equality_test = fair_metric(output, idx_test, labels, sens)
 
         if acc_val > best_acc:
             best_acc = acc_val
             best_test = acc_test
+            best_dp = parity_val
+            best_dp_test = parity_test
+            best_eo = equality_val
+            best_eo_test = equality_test
 
         print("Epoch {}:".format(epoch),
               "acc_test= {:.6f}".format(acc_test.item()),
               "acc_val: {:.6f}".format(acc_val.item()),
-              "dp_test: {:.6f}".format(parity),
-              "eo_test: {:.6f}".format(equality),
+              "dp_val: {:.6f}".format(parity_val),
+              "dp_test: {:.6f}".format(parity_test),
+              "eo_val: {:.6f}".format(equality_val),
+              "eo_test: {:.6f}".format(equality_test),
               "best_acc: {:.6f}".format(best_test))
-
-
-def fair_metric(output, idx, labels, sens):
-    val_y = labels[idx].cpu().numpy()
-    idx_s0 = sens.cpu().numpy()[idx.cpu().numpy()] == 0
-    idx_s1 = sens.cpu().numpy()[idx.cpu().numpy()] == 1
-
-    idx_s0_y1 = np.bitwise_and(idx_s0, val_y == 1)
-    idx_s1_y1 = np.bitwise_and(idx_s1, val_y == 1)
-
-    pred_y = (output[idx].squeeze() > 0).type_as(labels).cpu().numpy()
-    parity = abs(sum(pred_y[idx_s0]) / sum(idx_s0) - sum(pred_y[idx_s1]) / sum(idx_s1))
-    equality = abs(sum(pred_y[idx_s0_y1]) / sum(idx_s0_y1) - sum(pred_y[idx_s1_y1]) / sum(idx_s1_y1))
-
-    return parity, equality
+    print("Test results:",
+          "acc_test= {:.6f}".format(best_test.item()),
+          "acc_val: {:.6f}".format(best_acc.item()),
+          "dp_val: {:.6f}".format(best_dp),
+          "dp_test: {:.6f}".format(best_dp_test),
+          "eo_val: {:.6f}".format(best_eo),
+          "eo_test: {:.6f}".format(best_eo_test), )
 
 
 if __name__ == '__main__':
