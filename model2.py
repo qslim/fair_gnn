02207a503f3f0ed83d2b1493d62935dbc9ca2a3c
“qslim” <mqyang1s@163.com>
2023-10-19 15:50:05 +0800

    Test results: acc_test= 0.705725 acc_val: 0.728070 dp_val: 0.053976 dp_test: 0.005589 eo_val: 0.052893 eo_test: 0.001824

--- model2.py
+++ model2.py
@@ -31,14 +31,15 @@ class FeedForwardNetwork(nn.Module):
 
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(FeedForwardNetwork, self).__init__()
-        self.layer1 = nn.Linear(input_dim, hidden_dim)
-        self.gelu = nn.GELU()
-        self.layer2 = nn.Linear(hidden_dim, output_dim)
+        self.ffn = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            # nn.LayerNorm(hidden_dim),
+            nn.GELU(),
+            nn.Linear(hidden_dim, output_dim),
+        )
 
     def forward(self, x):
-        x = self.layer1(x)
-        x = self.gelu(x)
-        x = self.layer2(x)
+        x = self.ffn(x)
         return x
 
 
@@ -50,7 +51,8 @@ class SpecLayer(nn.Module):
         self.ffn = nn.Sequential(
             nn.Linear(hidden_dim, hidden_dim),
             nn.LayerNorm(hidden_dim),
-            nn.ReLU()
+            nn.GELU()
+            # nn.ReLU()
         )
 
     def forward(self, x):
